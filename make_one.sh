326f947501ae4a02e60bcddb06b0384adb14d085
anopaden <anopaden@cvut.cz>
2024-06-11 12:17:54 +0200

    delete all_in_one shell script for progtest.

--- make_one.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-grep -vh '^#include' ./pos/pos.h ./pos/pos.cpp ./spreadsheet/spreadsheet.h ./literals/literals.h ./literals/literals.cpp ./expr/expr.h ./expr/expr.cpp ./spreadsheet/spreadsheet.cpp > all_in_one.cpp
-
-sed -i  "s/#pragma once//g" all_in_one.cpp
-sed -i "107,117d" all_in_one.cpp
